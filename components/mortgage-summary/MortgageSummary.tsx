a38255a503828345a5058bd9740c3dc696c0477c
Jonathan Ly <windevly@gmail.com>
2022-09-08 22:16:33 -0500

    missed some renames

--- components/mortgage-summary/MortgageSummary.tsx
+++ components/mortgage-summary/MortgageSummary.tsx
@@ -26,32 +26,32 @@ export default function MortgageSummary(props: MortgageSummaryProps) {
     }
 
     function getHomeownerInsurance(): number {
-        return isNaN(Number(mortgage.homeOwnerInsurance)) ? 0 : Number(mortgage.homeOwnerInsurance)
+        return isNaN(Number(mortgage.HomeOwnerInsurance)) ? 0 : Number(mortgage.HomeOwnerInsurance)
     }
 
     function getPMI(): number {
-        const pmi = isNaN(Number(mortgage.privateMortgageInsurance)) ? 0 : Number(mortgage.privateMortgageInsurance)
+        const pmi = isNaN(Number(mortgage.PrivateMortgageInsurance)) ? 0 : Number(mortgage.PrivateMortgageInsurance)
 
-        return mortgage.downPaymentPercent ?? 0 >= 20 ? 0 : pmi
+        return mortgage.DownPaymentPercent ?? 0 >= 20 ? 0 : pmi
     }
 
     function getPropertyTax(): number {
-        return isNaN(Number(mortgage.propertyTax)) ? 0 : Number(mortgage.propertyTax)
+        return isNaN(Number(mortgage.PropertyTax)) ? 0 : Number(mortgage.PropertyTax)
     }
 
     function getPrincipalInterest(): number {
-        const loanAmount = (mortgage.homePrice ?? 0) - (mortgage.downPayment ?? 0)
-        const monthlyInterestPercent = (mortgage.interest ?? 0) / 100 / 12
+        const loanAmount = (mortgage.HomePrice ?? 0) - (mortgage.DownPayment ?? 0)
+        const monthlyInterestPercent = (mortgage.Interest ?? 0) / 100 / 12
 
         const result = loanAmount * 
-            (monthlyInterestPercent * Math.pow(1 + monthlyInterestPercent, (mortgage.term ?? 0) * 12)) / 
-            (Math.pow(1 + monthlyInterestPercent, (mortgage.term ?? 0) * 12) - 1)
+            (monthlyInterestPercent * Math.pow(1 + monthlyInterestPercent, (mortgage.Term ?? 0) * 12)) / 
+            (Math.pow(1 + monthlyInterestPercent, (mortgage.Term ?? 0) * 12) - 1)
 
         return isNaN(result) ? 0 : result
     }
 
     function getStartingInterest(): number {
-        return ((mortgage.interest ?? 0)/100/12) * ((mortgage.homePrice ?? 0) - (mortgage.downPayment ?? 0))
+        return ((mortgage.Interest ?? 0)/100/12) * ((mortgage.HomePrice ?? 0) - (mortgage.DownPayment ?? 0))
     }
 
     function getStartingPrincipal(): number {
